06f201b796a6e0be61fa6488b4820ffb9437a252
susangi-admin <susangi@mylinex.com>
2023-03-23 13:11:09 +0530

    "implement" : "add post form create"

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    public $content;
+    private mixed $title;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|unique:posts|max:100',
+            'content' => 'min:20'
+        ];
+    }
+}
